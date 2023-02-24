e6d73b8c2a9ad37f0c7785a6d14b09ee9fd4adbc
mila <milavoronina1997@mail.ru>
2023-02-24 11:58:59 +0500

    fix hw7

---
+++ Interfaces/__init__.py
@@ -0,0 +1,6 @@
+from .Params import *
+from Interfaces import Fly
+from Interfaces import Leader
+from Interfaces import MakeNoise
+from Interfaces import ShowAffection
+from Interfaces import Train
